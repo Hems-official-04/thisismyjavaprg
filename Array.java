e218bb178024e91bb314a92617d2ebcb0de3a539
Hems-official-04 <meetathemambbs@gmail.com>
2025-03-18 06:45:26 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,36 @@
+package addon;
+
+import java.util.*;
+
+
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		/*int []num = new int[5];
+
+		num[0] =10;
+
+		num[1] =20;
+
+		System.out.println(num[0]);//single dimensional array
+
+		int []num = new int[5];
+
+		num[0] =10;
+
+		num[1] =20;
+
+		for(int i=0;i<num.length;i++) {
+
+			System.out.println(num[i]);*/
+
+		}
+
+	}
+
+
+
+}
+
